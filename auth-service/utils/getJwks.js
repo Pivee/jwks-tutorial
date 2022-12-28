96bfb8ab3e1f0abadc2e4c9d79f687e48722c1b6
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-12-29 02:02:19 +0530

    Fix /jwks.json response

--- auth-service/utils/getJwks.js
+++ auth-service/utils/getJwks.js
@@ -1,7 +1,7 @@
 const pem2jwk = require("pem-jwk").pem2jwk;
 
 function getJwks(privateKey) {
-  return pem2jwk(privateKey, { use: "sig" }, "public");
+  return { keys: [pem2jwk(privateKey, { use: "sig" }, "public")] };
 }
 
 module.exports = {
